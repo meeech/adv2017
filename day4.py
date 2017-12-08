930f610489716a5a711c5f3dad381e1f1cb21aff
Mitchell Amihod <mamihod@shutterstock.com>
2017-12-08 09:40:17 -0500

    working day 4

--- day4.py
+++ day4.py
@@ -1,7 +1,20 @@
-import sys
+import itertools
 import re
-count = 0
+import sys
 badchars = re.compile('(?:[A-Z\d])')
+
+def has_anagram(pieces):
+    for tup in itertools.combinations(pieces, 2):
+        print(tup)
+        a = list(tup[0])
+        b = list(tup[1])
+        a.sort()
+        b.sort()
+        if a == b:
+            return True
+
+    return False
+
 
 def is_valid(_line):
 
@@ -13,17 +26,27 @@ def is_valid(_line):
     if badchars.search(_line):
         return False
 
-    return len(pieces) == len(set(pieces))
+    if len(pieces) != len(set(pieces)):
+        return False
 
-for line in sys.stdin:
-    if is_valid(line.replace('\n', '')):
-        count = count + 1
-    else:
-        print(line)
+    if has_anagram(pieces):
+        return False
+
+    return True
+
+
+def main():
+    count = 0
+    for line in sys.stdin:
+        if is_valid(line.replace('\n', '')):
+            count = count + 1
+        else:
+            print(line)
+    return count
 
 print(is_valid('aa bb cc dd ee') == True)
 print(is_valid('aa bb cc dd aa') == False)
 print(is_valid('aa bb cc dd aaa') == True)
 print(is_valid('aa Bb cc dd') == False)
 
-print(count)
+print(main())
