3fc361d92122b49fe6c79c252aa6d820a9924d0f
Mitchell Amihod <mamihod@shutterstock.com>
2017-12-11 13:01:28 -0500

    day5, bonus

--- day5.py
+++ day5.py
@@ -9,10 +9,16 @@ def main():
     while in_maze:
 
         to_move = lines[current_offset]
-        lines[current_offset] = lines[current_offset] + 1
+        offset_delta = 1
+        if to_move >= 3:
+            offset_delta = -1
+
+        lines[current_offset] = lines[current_offset] + offset_delta
 
         current_offset = current_offset + to_move
         steps = steps + 1
         in_maze =  0 <= current_offset < len(lines)
+
+    print(steps)
 
 main()
